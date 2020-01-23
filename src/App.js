49d6c2f31680a3d138ea44470f3392b9644be21c
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-01-23 11:49:18 -0300

    Sistema de Módulo: Import/Export

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { View, StyleSheet } from 'react-native'
 
 import Simples from './components/Simples'
 import ParImpar from './components/ParImpar'
+import Inverter, { MegaSena } from './components/Multi'
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Simples texto='Flexível!!'/>
-				<ParImpar numero={30}/>
+				<Simples texto='Flexível!!' />
+				<ParImpar numero={30} />
+				<Inverter texto='React Nativo' />
+				<MegaSena numeros={6}/>
 			</View>
 		);
 	}
