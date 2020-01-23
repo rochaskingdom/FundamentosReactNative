48662f8d27efaa4df993c0d792d97f66795ba2f2
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-01-23 10:20:12 -0300

    Criando Estilo

--- src/components/Simples.js
+++ src/components/Simples.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Text } from 'react-native'
+import Padrao from '../style/Padrao'
 
 // export default function(props) {
 //     return <Text>{props.texto}</Text>
 // }
 
-export default props => <Text>Arrow 1: {props.texto}</Text>
+export default props => 
+    <Text style={Padrao.ex}>Arrow 1: {props.texto}</Text>
 
 
 // 2 componentes entre a View
