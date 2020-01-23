493be79df8155d9f6d75903efa818a357502dab7
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-01-23 12:44:05 -0300

    Adicionando Navegação ao Projeto

---
+++ src/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { createDrawerNavigator } from 'react-navigation'
+
+import Simples from './components/Simples'
+import ParImpar from './components/ParImpar'
+import Inverter, { MegaSena } from './components/Multi'
+
+export default createDrawerNavigator({
+    MegaSena: {
+        screen: () => <MegaSena numeros={8} />,
+        navigationOptions: { title: 'Mega Sena' }
+    },
+    Inverter: {
+        screen: () => <Inverter texto ='React Nativo!' />
+    },
+    ParImpar: {
+        screen: () => <ParImpar numero={30} />,
+        navigationOptions: { title: 'Par & Ímpar' }
+    },
+    Simples: {
+        screen: () => <Simples texto='Flexível!!!' />
+    }
+}, { drawerWidth: 300 });
