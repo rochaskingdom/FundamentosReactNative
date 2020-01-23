6e90e8fa886a1038280406791841348a55371cb7
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-01-23 11:28:01 -0300

    Renderização Condicional

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+
 import Simples from './components/Simples'
+import ParImpar from './components/ParImpar'
 
 export default class App extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
 				<Simples texto='Flexível!!'/>
+				<ParImpar numero={30}/>
 			</View>
 		);
 	}
